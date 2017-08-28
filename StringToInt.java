d52d2234aac9eaefa8248bff86b1204a1ae94bc3
aumbisht <30600807+aumbisht@users.noreply.github.com>
2017-08-28 23:08:37 +0530

    code only for simple interger string

---
+++ StringToInt.java
@@ -0,0 +1,27 @@
+package puzzleSolving;
+
+public class StringToInteger {
+public static void main(String...s){
+ String st="-998";
+ int i=0;
+ double number=0;
+ boolean isNegative=false;
+      int len =st.length();//length of String
+      if(st.charAt(0)=='-'){
+    	  isNegative=true;
+      i=1;//if it is negative then '-'symbol placed at zero index and increase its index by one
+      
+ 
+}
+      //otherwise it is positive number so it starts from zero index
+      while(i<len){
+    	  number=number*10;//0*10=0 for decimal place
+    	  number=number+(st.charAt(i++)-'0');//original number(numeric value)=ascii representation of char-base ascii char for numeric is 0(0 having ascii value is 48) 
+    	  
+      }if(isNegative)
+    	  number=-number;//if it is negative in last add - symbol in starting
+      
+      System.out.println(number+998); //for testing purpose yet given string converted to integer
+	}
+
+}
